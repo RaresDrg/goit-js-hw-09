3e085acf2f4620685615d1b891802797972fb233
RaresDrg <dragan_rares13@yahoo.com>
2023-11-11 01:27:13 +0200

    ex 1  done
    ex 1  done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = '';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
